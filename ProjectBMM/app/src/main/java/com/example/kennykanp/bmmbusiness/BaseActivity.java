6f779d606cf4c52f85f1c98a864c639b4ee9a83a
Kenny Kanp <kkanp57@outlook.com>
2018-01-16 10:51:19 -0500

    This is better version

---
+++ ProjectBMM/app/src/main/java/com/example/kennykanp/bmmbusiness/BaseActivity.java
@@ -0,0 +1,35 @@
+package com.example.kennykanp.bmmbusiness;
+
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog(String msj) {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setProgressStyle(R.style.ProgressBar);
+            mProgressDialog.setMessage(msj);
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+}
